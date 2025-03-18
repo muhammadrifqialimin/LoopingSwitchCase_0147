1d9af6b944a5c9ad515dd4cbfb425c13b74a1944
Muhammad Rifqi Alimin <rifqi.alimin.ft23@mail.umy.ac.id>
2025-03-18 19:33:02 +0800

    commit 3

--- Looping.cpp
+++ Looping.cpp
@@ -17,3 +17,14 @@ int main()
     }
     cout << "Nilai n terakhir = " << n << endl;
     cout << endl;
+
+    cout << "Perulangan for pencacah turun" << endl;
+    // Perulangan dengan for
+    for (n = 5; n > 0; n--)
+    {
+        cout << "Nilai n = " << n << " Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n;
+    cout << endl;
+
+    cout << "Perulangan while" << endl;
